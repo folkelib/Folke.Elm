9e6b1e056df960e0591705848e61d62309558c74
Sidoine <sidoine@sidoine.net>
2015-01-05 16:26:22 +0100

    Colonne DateTime_Precision supprimée

--- Folke.Orm/InformationSchema/Columns.cs
+++ Folke.Orm/InformationSchema/Columns.cs
@@ -15,7 +15,7 @@
         public int? CHARACTER_OCTET_LENGTH { get; set; }
         public int? NUMERIC_PRECISION { get; set; }
         public int? NUMERIC_SCALE { get; set; }
-        public int? DATETIME_PRECISION { get; set; }
+       // public int? DATETIME_PRECISION { get; set; }
         public string CHARACTER_SET_NAME { get; set; }
         public string COLLATION_NAME { get; set; }
         public string COLUMN_TYPE { get; set; }
