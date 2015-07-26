705ca9bae3b6be50754fdd662892dce335dbf00b
Sidoine De Wispelaere <sidoine@sidoine.net>
2015-07-27 00:36:06 +0200

    Add a new many to many helper

--- Folke.Orm/ManyToManyHelpers.cs
+++ Folke.Orm/ManyToManyHelpers.cs
@@ -24,6 +24,7 @@ namespace Folke.Orm
             return connection.UpdateManyToMany(parent, currentValues, newDtos, new FolkeTableManyToManyHelper<TChild, TChildDto>(mapper));
         }
 
+        
         public static T MustExist<T>() where T : class
         {
             throw new Exception("The item does not exist");
@@ -61,6 +62,38 @@ namespace Folke.Orm
             }
         }
 
+        public static IReadOnlyList<T> UpdateManyToMany<T, TParent, TChild>(this IFolkeConnection connection, TParent parent, IReadOnlyList<T> currentValues, ICollection<int> newValueIds)
+            where T : class, IManyToManyTable<TParent, TChild>, new()
+            where TParent : IFolkeTable
+            where TChild : class, IFolkeTable, new()
+        {
+            return connection.UpdateManyToMany(parent, currentValues, newValueIds, new FolkeTableManyToManyHelperWithIds<TChild>());
+        }
+
+        private class FolkeTableManyToManyHelperWithIds<TChild> :
+            IManyToManyHelperConfiguration<TChild, int> where TChild : class, IFolkeTable, new()
+        {
+            public bool AreEqual(TChild child, int dto)
+            {
+                return child.Id == dto;
+            }
+
+            public TChild Map(int dto)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IList<TChild> QueryExisting(IFolkeConnection connection, IList<int> dto)
+            {
+                return dto.Any() ? connection.QueryOver<TChild>().Where(c => c.Id.In(dto)).List() : null;
+            }
+
+            public void UpdateDto(int dto, TChild child)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         public static IReadOnlyList<T> UpdateManyToMany<T, TParent, TChild, TChildDto>(this IFolkeConnection connection, TParent parent, IReadOnlyList<T> currentValues, ICollection<TChildDto> newDtos, IManyToManyHelperConfiguration<TChild, TChildDto> helper)
             where T : class, IManyToManyTable<TParent, TChild>, new()
             where TParent : IFolkeTable
@@ -69,10 +102,12 @@ namespace Folke.Orm
             if (newDtos == null)
                 newDtos = new List<TChildDto>();
 
+            // Looking for any value in newDtos that is not in currentValues
             var valuesToAdd = newDtos.Where(v => currentValues == null || !currentValues.Any(cv => helper.AreEqual(cv.Child, v))).ToList();
             var newValues = currentValues == null ? new List<T>() : currentValues.ToList();
             if (valuesToAdd.Any())
             {
+                // Query from the database the values that needs to be added to the parent
                 var existingChildren = helper.QueryExisting(connection, valuesToAdd);
 
                 foreach (var newDto in valuesToAdd)
@@ -80,11 +115,13 @@ namespace Folke.Orm
                     var child = existingChildren == null ? null : existingChildren.SingleOrDefault(c => helper.AreEqual(c, newDto));
                     if (child == null)
                     {
+                        // If the element to add does not exist in the database, create it (may fail if helper.Map/helper.UpdateDto is not implemented because one does not want to allow that)
                         child = helper.Map(newDto);
                         connection.Save(child);
                         helper.UpdateDto(newDto, child);
                     }
 
+                    // Create a new parent-child link with this value
                     var newElement = new T { Child = child, Parent = parent };
                     connection.Save(newElement);
                     newValues.Add(newElement);
@@ -93,6 +130,7 @@ namespace Folke.Orm
             
             if (currentValues != null)
             {
+                // Look for values to remove
                 var valuesToRemove = currentValues.Where(cv => newDtos.All(nv => !helper.AreEqual(cv.Child, nv))).ToArray();
                 if (valuesToRemove.Any())
                 {
