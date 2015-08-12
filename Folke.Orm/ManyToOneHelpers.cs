d702aaeacb8b6eb7f4bfc76157f28c93c6225002
Sidoine De Wispelaere <sidoine@sidoine.net>
2015-08-12 13:10:46 +0200

    Add a new UpdateCollection helper

--- Folke.Orm/ManyToOneHelpers.cs
+++ Folke.Orm/ManyToOneHelpers.cs
@@ -6,6 +6,16 @@ namespace Folke.Orm
 {
     public static class ManyToOneHelpers
     {
+        /// <summary>
+        /// Updates a collection of items from a collection of item views. Existing items are updated or deleted. New items are added.
+        /// </summary>
+        /// <typeparam name="TChild">The database item</typeparam>
+        /// <typeparam name="TChildView">A view of this database item. Matching items have same Id</typeparam>
+        /// <param name="connection">The database connection</param>
+        /// <param name="currentValues">The current value from the database</param>
+        /// <param name="newValues">The new values</param>
+        /// <param name="factory">A factory that create a new item</param>
+        /// <param name="updater">A delegate that updates an existing item</param>
         public static void UpdateCollection<TChild, TChildView>(this IFolkeConnection connection, IEnumerable<TChild> currentValues, IEnumerable<TChildView> newValues,
             Func<TChildView, TChild> factory, Func<TChildView, TChild, bool> updater)
             where TChild: class, IFolkeTable, new()
@@ -23,6 +33,38 @@ namespace Folke.Orm
                 {
                     if (updater(newValue, currentValue))
                         connection.Update(currentValue);
+                }
+            }
+
+            foreach (var childDto in newValueToAdd)
+            {
+                var child = factory(childDto);
+                connection.Save(child);
+            }
+        }
+
+        /// <summary>
+        /// Update a collection of items using a collection of values. These values don't have an id to identity them, so a areEqual delegate
+        /// must be given as a parameter. Existing items that is not in newValues are deleted. 
+        /// </summary>
+        /// <typeparam name="TChild">The database item type</typeparam>
+        /// <typeparam name="TChildView">The view type</typeparam>
+        /// <param name="connection">The database connection</param>
+        /// <param name="currentValues">The current value from the database</param>
+        /// <param name="newValues">The new values</param>
+        /// <param name="areEqual">Must return true if the two values are equal</param>
+        /// <param name="factory">Create a new item</param>
+        public static void UpdateCollection<TChild, TChildView>(this IFolkeConnection connection, IEnumerable<TChild> currentValues, IEnumerable<TChildView> newValues,
+            Func<TChildView, TChild, bool> areEqual, Func<TChildView, TChild> factory)
+            where TChild : class, IFolkeTable, new()
+        {
+            var newValueToAdd = newValues.Where(x => currentValues.All(y => !areEqual(x, y)));
+            foreach (var currentValue in currentValues)
+            {
+                var newValue = newValues.FirstOrDefault(x => areEqual(x, currentValue));
+                if (newValue == null)
+                {
+                    connection.Delete(currentValue);
                 }
             }
 
