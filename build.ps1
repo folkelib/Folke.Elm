15d30f642a345acf24b4ace422fc769c957517e1
Sidoine De Wispelaere <sidoine@sidoine.net>
2015-10-23 15:09:54 +0200

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,5 @@
+param([String]$key)
+
 $span = New-TimeSpan -Start 2010/1/1 -End (Get-Date)
 $version = "beta-" + $span.Days.ToString() + ($span.Hours * 60 + $span.Minutes).ToString("0000")
 $env:DNX_BUILD_VERSION = $version
@@ -5,17 +7,17 @@ $env:DNX_BUILD_VERSION = $version
 cd .\src\Folke.Elm
 & dnu pack
 $file = Get-Item "bin\Debug\*-$version.nupkg"
-nuget push $file.FullName 4429cea5-062d-4e73-8b1b-7a593d988f06
+nuget push $file.FullName $key 
 cd ..\..
 
 cd .\src\Folke.Elm.Mysql
 & dnu pack
 $file = Get-Item "bin\Debug\*-$version.nupkg"
-nuget push $file.FullName 4429cea5-062d-4e73-8b1b-7a593d988f06
+nuget push $file.FullName $key 
 cd ..\..
 
 cd .\src\Folke.Elm.Sqlite
 & dnu pack
 $file = Get-Item "bin\Debug\*-$version.nupkg"
-nuget push $file.FullName 4429cea5-062d-4e73-8b1b-7a593d988f06
+nuget push $file.FullName $key
 cd ..\..
