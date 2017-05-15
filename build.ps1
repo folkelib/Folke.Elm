58cd37fe314ca55b2f00fd0a493ac6841f4f5ce5
Sidoine De Wispelaere <sdewispelaere@talentsoft.com>
2017-05-15 11:53:41 +0200

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,48 +1,6 @@
 param([String]$key,[String]$version)
 
-function setProjectVersion([String]$fileName, [String]$version) {
-    $content = (Get-Content $fileName) -join "`n" | ConvertFrom-Json
-    $content.version = $version
-    $newContent = ConvertTo-Json -Depth 10 $content
-    Set-Content $fileName $newContent
-}
-
 if ($version -ne "") {
-	setProjectVersion ".\src\Folke.Elm\project.json" $version
-	setProjectVersion ".\src\Folke.Elm.Mysql\project.json" $version
-	setProjectVersion ".\src\Folke.Elm.Sqlite\project.json" $version
-	setProjectVersion ".\src\Folke.Elm.MicrosoftSqlServer\project.json" $version
-	setProjectVersion ".\src\Folke.Elm.PostgreSql\project.json" $version
-
-	& dotnet restore
-
-	cd .\src\Folke.Elm
-	& dotnet pack -c Release
-	$file = Get-Item "bin\Release\*.$version.nupkg"
-	nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
-	cd ..\..
-
-	cd .\src\Folke.Elm.Mysql
-	& dotnet pack -c Release
-	$file = Get-Item "bin\Release\*.$version.nupkg"
-	nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
-	cd ..\..
-
-	cd .\src\Folke.Elm.Sqlite
-	& dotnet pack -c Release
-	$file = Get-Item "bin\Release\*.$version.nupkg"
-	nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
-	cd ..\..
-
-	cd .\src\Folke.Elm.MicrosoftSqlServer
-	& dotnet pack -c Release
-	$file = Get-Item "bin\Release\*.$version.nupkg"
-	nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
-	cd ..\..
-
-	cd .\src\Folke.Elm.PostgreSql
-	& dotnet pack -c Release
-	$file = Get-Item "bin\Release\*.$version.nupkg"
-	nuget push $file.FullName $key -Source https://api.nuget.org/v3/index.json
-	cd ..\..
+	nuget install Folke.Build -ExcludeVersion
+	& .\Folke.Build\tools\build.ps1 $key $version
 }
